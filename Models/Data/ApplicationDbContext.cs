5de3ce008839666385f576a38e3fc92ccf3b17f8
ajit3676 <167732185+ajit3676@users.noreply.github.com>
2024-05-10 07:13:15 +1000

    Keep changing

--- Models/Data/ApplicationDbContext.cs
+++ Models/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Restaurant.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
--- Models/Data/Book.cs
+++ Models/Data/Book.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace HamroLibrary.Models.Data
+namespace Restaurant.Models.Data
 {
     public class Book
     {
