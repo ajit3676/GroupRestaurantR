7650c45c79664ffe2b96ec65075fee35e1a558c5
ajit3676 <167732185+ajit3676@users.noreply.github.com>
2024-05-10 18:32:59 +1000

    reservation model class
    Lalpek tutorial

---
+++ Models/Data/Reservation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HamroLibrary.Models.Data
+{
+    public class Reservation
+    {
+        public int ReservationID { get; set; }
+        public string ReservationName { get; set;}
+        public int NoOfGuests { get; set;}
+        public string ReservationDate { get; set;}
+        public string ReservationTime { get; set;}
+    }
+}
